ad8d96c41620dc2e1539eb28d78e44e4874605f5
sidetlw <tlw.isit@gmail.com>
2016-02-01 16:14:00 +0800

    全屏手势返回改为采用navigationVC自动动画

---
+++ Homepwner/testViewController.h
@@ -0,0 +1,13 @@
+//
+//  testViewController.h
+//  Homepwner
+//
+//  Created by test on 16/1/24.
+//  Copyright © 2016年 Mrtang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testViewController : UIViewController
+
+@end
