30d7e89c0733a876ffbc6e5e371a43335ced73ed
sidetlw <tlw.isit@gmail.com>
2016-02-03 14:31:52 +0800

    为被presentViewController出的VC添加屏幕边缘返回手势，被自定义dismiss返回动画

---
+++ Homepwner/BNRNavDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRNavDetailViewController.h
+//  Homepwner
+//
+//  Created by test on 16/2/3.
+//  Copyright © 2016年 Mrtang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRNavDetailViewController : UINavigationController
+
+@end
